6b9d698468b9ae1136303822aa395a07272321f4
Elden Modder <90405810+Cyphpython@users.noreply.github.com>
2024-10-03 16:06:02 +0200

    Updates Files
    Files now usable with example

--- Drapeau Hollandais/Drapeau Hollandais.cpp
+++ Drapeau Hollandais/Drapeau Hollandais.cpp
@@ -1,43 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void Tri()
+void Tri(vector <int> & arr)
 {
-    enum color { bleu, rouge, blanc };
-    int b, w, r;
-    int t;
-    int tab[999999999] = {};
-    int n = sizeof(tab) / sizeof(tab[0]);
-    // Initialisation des index
-    b = 0; // Index de la zone bleue
-    w = 0; // Index du premier élément non trié
-    r = n - 1; // Index de la zone rouge
-
+    enum color { bleu = 0, blanc = 1, rouge = 2 };
+    int b = 0;
+    int w = 0;
+    int r = arr.size() - 1;
     // Boucle principale, tant qu'on n'a pas parcouru tous les éléments
     while (w <= r) {
-        if (tab[w] == blanc) {
+        if (arr[w] == blanc) {
             // Si l'élément courant est blanc, on avance simplement
             w++;
         }
-        else if (tab[w] == bleu) {
+        else if (arr[w] == bleu) {
             // Si l'élément courant est bleu, on l'échange avec l'élément à l'index b
-            swap(tab[b], tab[w]);
+            swap(arr[b], arr[w]);
             b++; // On avance l'index bleu
             w++; // On avance l'index blanc également
         }
-        else if (tab[w] == rouge) {
+        else if (arr[w] == rouge) {
             // Si l'élément courant est rouge, on l'échange avec l'élément à l'index r
-            swap(tab[w], tab[r]);
+            swap(arr[w], arr[r]);
             r--; // On recule l'index rouge
             // On ne fait pas avancer w ici car l'élément échangé doit être vérifié
         }
     }
 }
 
-int main() 
+int main()
 {
-	Tri();
-	return 0;
+    // Exemple d'utilisation avec un tableau contenant des valeurs 0, 1, et 2
+    vector<int> arr = { 2, 0, 1, 2, 1, 0, 0, 1, 2 };
+
+    cout << "Tableau avant tri: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    Tri(arr);
+
+    cout << "Tableau apres tri: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
 }
